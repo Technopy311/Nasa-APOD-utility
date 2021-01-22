b4a73a2303b4d140a0983525c87f7a308e4dcb86
Technopy311 <xmenmagnet@gmail.com>
2021-01-22 01:18:18 -0300

    made requirements.txt and updated main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 import requests
 import json
 from pynotifier import Notification
-from os import system
+from os import system, path
+
 
 
 
@@ -39,7 +40,7 @@ def downloadBackground():
 	Notification(
 		title="Image succesfully changed to today's APOD ;D",
 		description="The background image has been succesfully changed to today's APOD.",
-		icon_path="/home/technopy/Documentos/programming/python/nasaAPOD/python-logo.png",
+		icon_path=path.join("python-logo.png"),
 		duration=2,
 		urgency=Notification.URGENCY_CRITICAL
 	).send()
@@ -47,7 +48,7 @@ def downloadBackground():
 	system("echo 'Image downloaded succesfully' >> /home/technopy/Documentos/programming/python/nasaAPOD/log.txt")
 
 
-def main():
+def loadAll():
 	print("INITIALIZING")
 	print("Loading the api data")
 	URL = 'https://api.nasa.gov/planetary/apod?api_key=F6wkuYMeExyyZW9516419T3pGXzHcA5mkEwkNTF8'
@@ -74,8 +75,41 @@ def main():
 	# parsing the json and 
 	global JSON_DATA 
 	JSON_DATA = json.loads(DATA)
+	
+
+def main():
+	loadAll()
+	#options for user
+	
+	print("What would you like to do? \n")
+	print("(1)Make first setup,    (2) Change your current background,    (3) Get all info of today image,\n"
+			"(4)Only Download low resolution image,    (5)Only Download high resolution image\n")
+	
+	option = input(": ")
+
+	option = int(option)
+
+	if option == 1:
+		print("Option selected is: " +str(option))
+
+	elif option == 2:
+		print("Option selected is: " +str(option))
+	elif option == 3:
+		print("Option selected is: " +str(option))
+	elif option == 4:
+		print("Option selected is: " +str(option))
+	elif option == 5:
+		print("Option selected is: " +str(option))
+	else:
+		print("That option doesn't exists or")
+		print("something else went wrong")
+		exit()
 
 
 if __name__ == '__main__':
-	main()
+	try:
+		main()
+	except KeyboardInterrupt:
+		print("Bye ;D")
+		exit()
 
