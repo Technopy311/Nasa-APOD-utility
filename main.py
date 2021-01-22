686749a4888093dc5dc0ad2b0476d5da4dd63cfc
Technopy311 <xmenmagnet@gmail.com>
2021-01-22 01:58:23 -0300

    Fixed the notifications error and starting to implement more functions.

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 import requests
 import json
 from pynotifier import Notification
-from os import system, path
+import os
 from time import sleep
 
 
-pauseTime = (1)
+PAUSE_TIME = (1)
 
 
 
@@ -27,31 +27,34 @@ def downloadBackground():
 	print("The date is: "+ str(date) +"\n\n")
 	
 	#a little pause.
-	sleep(pauseTime)
+	sleep(PAUSE_TIME)
 
 	#Starting the download process of the image
 	print('Downloading the image. \n\n')
-	sleep(pauseTime)
+	sleep(PAUSE_TIME)
 	
-	system('wget '+ str(url) + ' --quiet  -O \"APOD.jpg\" ')
+	os.system('wget '+ str(url) + ' --quiet  -O \"APOD.jpg\" ')
 	print('Image Downloaded')
 
 	#setting the image as background
 
 	print("Setting as a background.")
-	system("mv /home/technopy/Documentos/programming/python/nasaAPOD/APOD.jpg /home/technopy/.local/share/backgrounds/default.jpg")
+	os.system("mv /home/technopy/Documentos/programming/python/nasaAPOD/APOD.jpg /home/technopy/.local/share/backgrounds/default.jpg")
 	print("succesfully changed background.")
 	
+	currentDirectory = os.getcwd()
+
 	#Displaying a notification	
 	Notification(
 		title="Image succesfully changed to today's APOD ;D",
 		description="The background image has been succesfully changed to today's APOD.",
-		icon_path=path.join("/Assets","python-logo.png"),
+		icon_path=os.path.join(currentDirectory, "Assets/python-logo.png"),
 		duration=2,
-		urgency=Notification.URGENCY_CRITICAL
+		urgency=Notification.URGENCY_NORMAL,
+
 	).send()
 
-	system("echo 'Image downloaded succesfully' >> /home/technopy/Documentos/programming/python/nasaAPOD/log.txt")
+	os.system("echo 'Image downloaded succesfully' >> /home/technopy/Documentos/programming/python/nasaAPOD/log.txt")
 
 
 def loadAll():
@@ -81,7 +84,11 @@ def loadAll():
 	# parsing the json and 
 	global JSON_DATA 
 	JSON_DATA = json.loads(DATA)
-	
+
+
+def setup():
+	pass
+
 
 def main():
 
@@ -91,7 +98,7 @@ def main():
 	#options for the user
 	
 	print("What would you like to do? \n")
-	print("(1)Make rst setup,    (2) Change your current background,    (3) Get all info of today image,\n"
+	print("(1)Make first setup,    (2) Change your current background,    (3) Get all info of today image,\n"
 			"(4)Only Download low resolution image,    (5)Only Download high resolution image\n")
 	
 	option = input(": ")
