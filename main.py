021296fe229868a55ac70b94974d0f13536e35c2
Technopy311 <xmenmagnet@gmail.com>
2021-01-23 02:14:26 -0300

    Succesfully made the project versatile and complete v2

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ from time import sleep
 PAUSE_TIME = (2)
 
 def loadAll():
+	global background_dir 
+	global currentDirectory 
+
+	currentDirectory = os.getcwd()
+
+	conf = open((currentDirectory + "/Assets/path.conf"), 'w')
+	conf.write(" ")
+	conf.close()
+	
 	print("INITIALIZING")
 	print("Loading the api data")
 	URL = 'https://api.nasa.gov/planetary/apod?api_key=F6wkuYMeExyyZW9516419T3pGXzHcA5mkEwkNTF8'
@@ -23,6 +32,10 @@ def loadAll():
 	request = requests.get(URL)
 	status = request.status_code
 	DATA = request.text
+	
+	background_dir = open((currentDirectory + "/Assets/path.conf"), 'r')
+	background_dir = background_dir.readline()
+
 
 	# checking connection
 	if status == 200:
@@ -132,8 +145,11 @@ def jk():
 
 
 def downloadBackground():
-	#Getting the current directory
-	currentDirectory = os.getcwd()
+	global currentDirectory
+
+	background_dir = open((currentDirectory + "/Assets/path.conf"), 'r')
+	background_dir = background_dir.readline()
+
 
 	#Getting the info from json
 
@@ -149,7 +165,8 @@ def downloadBackground():
 	#setting the image as background
 
 	print("Setting as a background.")
-	os.system()
+	
+	os.system("mv " + (currentDirectory + "/APOD.jpg ") + background_dir)
 	print("succesfully changed background.")
 
 	#Displaying a notification	
@@ -167,46 +184,98 @@ def downloadBackground():
 	file.close()
 
 
+def setup1():
+	
+	currentDirectory = os.getcwd()
+	currentDirectory = str(currentDirectory)
+	save_file = (currentDirectory + "/Assets/path.conf")
+
+	print("Be conscient that your current wallpaper will be replaced, ")
+	print("So make a copy of it, if you wana save it.")
+	print("let's continue: \n")
+	
+	print("Input here, the ABSOLUTE PATH of your actual desktop wallpaper \n"
+	"you must include the name of the file, "
+	"Example: /home/USERNAME/Desktop/wallpaper.jpg \n")
+
+	path = input("Enter the path: ")
+	print("make shure its the right path, else,\n just run or re run the option 1.")
+
+	save = open(save_file, 'w' )
+	save.write(path)
+	save.close()
+	
+	print("the configuration is done, would you like now to change your background? (1)Y / (2)N")
+	SecondOption = input(": ")
+	SecondOption = int(SecondOption)
+
+	if SecondOption == 1:
+		downloadBackground()
+	else:
+		jk()
+		exit()
+
+
+
+def setup2():
+
+	print("This option consists in download the image,\n"
+	" and manually set it as background.")
+
+	print("The helper has pre-programmed pauses, of maximum 30secs,"
+	"so don't think that the script has been freezed or stoped.")
+
+	print("Follow the steps, ill show you here")
+	sleep(10)
+	print("Ill download the image for you, wait a little and dont do nothing. \n")
+	sleep(3)
+	DownloadHD()
+	print("\n Ok, the image has been downloaded and saved")
+	print("In your current directory as High-Resolution-Img.jpg")
+	sleep(4)
+	print("you can move the file wherever you want, but copy the path to it")
+	print("Example: /home/USERNAME/Desktop/anyname.jpg")
+	sleep(PAUSE_TIME)
+	print("yes, it must include the name of the file.")
+	sleep(10)
+	print("Once you made all that, just make it as your background wallpaper, \n"
+	"in fact, is not neccesary to inmediatly make it as your wallpaper, "
+	"and you can make it before the script ends, but you must do it \n"
+	"Else, when you run the automatized script, it wont work 100%. \n\n")
+	print("Here come the pause of 30 secs \n")
+	sleep(30)
+	print("If you did it, now the only missing thing to do, is to ")
+	print("enter the path where the file is, that way")
+	sleep(PAUSE_TIME)
+	print("The next time you execute the script, you only have to choose the ")
+	print("Option 1 \n")
+	sleep(PAUSE_TIME)
+
+	setup1()
 
 def InitialSetup():
-	print("Initializing... \n")
-	print("here are two options of setup")
 	
-	print("(1) Rename your actual wallpaper file.\n"
+
+	print("Initializing... \n")
+	
+
+	print("here are two options of setup")
+	print("(1) Replace your actual wallpaper file.\n"
 		"(2) Download the image, and set it as wallapaper.")
 
-	print("This are only short descriptions")
+	print("Disclaimer, This are only short descriptions, "
+	"if you wish to stop the script, just type CTRL + C. \n")
+	
 	print("What do you choose?")
 
 	option = input(": ")
 	option = int(option)
 	
 	if option == 1:
-		print("Input here, the ABSOLUTE PATH of yout actual desktop wallpaper")
-		PATH = input(": ")
-		print("make shure its the right path, else, just re run the script.")
-		
-		currentDirectory = os.getcwd()
-		save_file = os.path.join(currentDirectory, "Assets")
-		save_file = str(save_file)
-
-		save = open((save_file + "path.conf"), 'w')
-
-		save.write(PATH)
-		
-		print("the configuration is done, would you like to change your background? Y/N")
-		option = input(": ")
-
-		if option == 'Y' or option == 'y':
-			pass
-		else:
-			print("Ok, cya")
-			sleep(PAUSE_TIME)
-			exit()
+		setup1()
 	
-
 	elif option == 2:
-		pass
+		setup2()
 
 	else:
 		print("That option doesn't exists or something else went wrong")
