9ac257b264544bd722fcfe3a3721206f2a9ee102
Technopy311 <xmenmagnet@gmail.com>
2021-01-28 19:10:47 -0300

    Changed the for cron-job script, and updated .gitignore

--- change-background.py
+++ change-background.py
@@ -1,26 +1,10 @@
 import os
 import pynotifier
-from main import loadAll, downloadBackground, resetConf, current
+from main import loadAll, downloadBackground
 
-currentDirectory = current()
-
-path_file = (currentDirectory + "/Assets/path.conf")
-key_file = (currentDirectory + "/Assets/API-KEY.conf")
-
-path = "/home/technopy/.local/share/backgrounds/default.jpg"
-
-save_path = open(path_file, 'w' )
-save_path.write(path)
-save_path.close()
-
-key = "F6wkuYMeExyyZW9516419T3pGXzHcA5mkEwkNTF8"
-
-save_key = open(key_file, 'w' )
-save_key.write(key)
-save_key.close()
+#Downloading, Changing and setting up the new background.
 
 loadAll()
 
 downloadBackground()
 
-resetConf()
